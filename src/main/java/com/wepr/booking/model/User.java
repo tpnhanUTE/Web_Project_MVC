5f0b03d8d79e6d3069dc5f06a94fd48db96d8d0e
TanPhuc-GH <truongtanphuc2002@gmail.com>
2022-11-18 21:22:10 +0700

    updatedb

--- src/main/java/com/wepr/booking/model/User.java
+++ src/main/java/com/wepr/booking/model/User.java
@@ -1,19 +1,37 @@
 package com.wepr.booking.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+
 @Entity
 @Table
 public class User {
     @Id
     @GeneratedValue
-    public Integer User_ID;
+    private Integer User_ID;
     @Column
-    public String User_Name;
+    private String User_Name;
+    private String Email;
+    private String Password;
+    private String User_Avatar_Url;
+    private String User_Address;
+    private String User_Phone;
+    private String First_Name;
+    private String Last_Name;
+    //@JoinColumn(Decentralization_ID)
+    private Integer Decentralization_ID;
 
+    public Integer getUser_ID(){
+        return User_ID;
+    }
 
+    public void setUser_ID(Integer user_ID) {
+        this.User_ID = user_ID;
+    }
 
+    public  String getUser_Name(){
+        return User_Name;
+    }
+    public void setUser_Name(String user_Name){
+        this.User_Name = user_Name;
+    }
 }
