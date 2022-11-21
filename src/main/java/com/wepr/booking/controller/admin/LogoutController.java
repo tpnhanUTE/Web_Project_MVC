050e8f6ca3fd9931107585ba9801622253097a38
TanPhuc-GH <truongtanphuc2002@gmail.com>
2022-11-21 21:56:27 +0700

    fix javax

--- src/main/java/com/wepr/booking/controller/admin/LogoutController.java
+++ src/main/java/com/wepr/booking/controller/admin/LogoutController.java
@@ -1,9 +1,13 @@
 package com.wepr.booking.controller.admin;
 
-import jakarta.servlet.*;
-import jakarta.servlet.http.*;
-import jakarta.servlet.annotation.*;
 
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "logout", urlPatterns = "/logout")
