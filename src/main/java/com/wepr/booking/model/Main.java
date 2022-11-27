68d9f53423ec67e7a3ebb5b6b0982a39ad3fae22
tpnhan12a1 <76830997+tpnhan12a1@users.noreply.github.com>
2022-11-27 14:26:16 +0700

    add main

--- src/main/java/com/wepr/booking/model/Main.java
+++ src/main/java/com/wepr/booking/model/Main.java
@@ -1,5 +1,7 @@
 package com.wepr.booking.model;
 
+import com.wepr.booking.dao.PlaceDAO;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -13,10 +15,37 @@ public class Main {
 
 
 
-        //Catalog
-
+        //User
         User user = new User();
         em.persist(user);
+
+        Catalog catalog = new Catalog();
+        em.persist(catalog);
+
+        Place place = new Place();
+        em.persist(place);
+
+        Tour tour  = new Tour();
+        em.persist(tour);
+
+        Tour_Image tour_image = new Tour_Image();
+        em.persist(tour_image);
+
+        Payment payment = new Payment();
+        em.persist(payment);
+
+        Tour_Catalog tour_catalog = new Tour_Catalog();
+        em.persist(tour_catalog);
+
+        User_Tour_Comment user_tour_comment = new User_Tour_Comment();
+        em.persist(user_tour_comment);
+
+        User_Tour_Favorite user_tour_favorite = new User_Tour_Favorite();
+        em.persist(user_tour_favorite);
+
+        User_Tour_Book userTourBook = new User_Tour_Book();
+        em.persist(userTourBook);
+
         transaction.commit();
 
         em.close();
