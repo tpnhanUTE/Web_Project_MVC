02a210c9ad6d280c04aed6b9537c76e5b1df5893
tpnhan12a1 <76830997+tpnhan12a1@users.noreply.github.com>
2022-12-04 15:09:26 +0700

    fixx bug cart select

--- src/main/java/com/wepr/booking/controller/admin/PaymentController.java
+++ src/main/java/com/wepr/booking/controller/admin/PaymentController.java
@@ -34,7 +34,7 @@ public class PaymentController extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
         String[] checkTours = request.getParameterValues("checkTour");
-        if(checkTours ==null){
+        if(checkTours ==null || checkTours.length ==0){
             String url = "/cart";
             getServletContext().getRequestDispatcher(url).forward(request,response);
         }
